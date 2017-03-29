d63b5ca37c13b0aa67779ebc4196c4164edfcd3b
zblog <pokorzhang@gmail.com>
2017-03-29 18:25:15 +0800

    增加修改的文件3

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="author" content="Prakasam Mathaiyan">
     <meta name="description" content="">
-
     <!--[if IE]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -21,9 +19,7 @@
     <link rel="stylesheet" type="text/css" href="/css/main.css">
     <link rel="stylesheet" type="text/css" href="/css/style-default.css">
 </head>
-
 <body class="login2">
-
 <!-- Login Div Start Here -->
 <div class="login animated flipInY" id="logindiv">
     <div class="text-center logo">
@@ -31,7 +27,6 @@
            后台登录管理
        </h3>
     </div>
-
     <form role="form" class="loginForm" method="post" action="/admin/login">
 
             {{csrf_field()}}
@@ -51,65 +46,15 @@
                 <input type="password" class="form-control" name="txt_password" placeholder="密码">
             </div>
         </div>
-
-
-
         <div class="clearfix">
             <button type="submit" class="btn btn-primary pull-right">登录</button>
         </div>
-
         <div class="clearfix mt-md" style="display: none">
             如果你没有帐号点击 <a href="javascript:;" class="lnkRegister">注册!</a>
         </div>
     </form>
 
 </div>
-<!-- Login Div Ends Here -->
-
-
-<!-- Forgot Div End Here -->
-
-{{--<!-- Register Div Start Here -->
-<div class="login animated flipInY" id="registerDiv">
-    <div class="text-center logo">
-        <h3 style="color: #5bc0de">后台登录管理</h3>
-    </div>
-
-    <form role="form" class="registerForm" method="post" action="/admin/register">
-        {{csrf_field()}}
-        <div class="form-group">
-            <div class="input-icon">
-                <input type="text" class="form-control" name="register_fulName" placeholder="用户名">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="input-icon">
-                <input type="email" class="form-control" name="register_email" placeholder="用户名">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="input-icon">
-                <input type="password" class="form-control" name="forgot_password" placeholder="密码">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="input-icon">
-                <input type="password" class="form-control" name="forgot_cpassword" placeholder="确认密码">
-            </div>
-        </div>
-
-        <div class="clearfix">
-            <button type="button" class="btn btn-danger pull-left lnkLogin">返回</button>
-            <button type="submit" class="btn btn-primary pull-right">注册</button>
-        </div>
-
-    </form>
-</div>--}}
-
-
 
 </body>
 </html>
